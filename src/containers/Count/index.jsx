909ed95849192e32f9ec4b7bbc66647a4c210fc0
伍森 <56302170+Godboy5@users.noreply.github.com>
2022-08-23 14:09:28 +0800

    简写优化connect

--- src/containers/Count/index.jsx
+++ src/containers/Count/index.jsx
@@ -14,21 +14,24 @@ import {add,reduction,asynchronous} from '../../redux/count_action'
  * @description: connect第一个括号的参数 是一个函数状态  另一个书函数方法
  * @return 
  */
-function a(state){
-    return  {count:state}
-}
-function b(dispatch){
-    return {
-        jia:(data)=>{
-           dispatch(add(data))
-        },
-        jian:(data)=>{
-            dispatch(reduction(data))
-         },
-         asyn:(data,time)=>{
-            dispatch(asynchronous(data,time))
-         },
-    }
+// function a(state){
+//     return  {count:state}
+// }
+// function b(dispatch){
+//     return {
+//         jia:(data)=>{
+//            dispatch(add(data))
+//         },
+//         jian:(data)=>{
+//             dispatch(reduction(data))
+//          },
+//          asyn:(data,time)=>{
+//             dispatch(asynchronous(data,time))
+//          },
+//     }
     
-}
-export default connect(a,b)(CountUI)
+// }
+export default connect({count:state},
+                       {jia:add,
+                       jian:reduction,
+                       asyn:asynchronous})(CountUI)
