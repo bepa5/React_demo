628d32206930448f5b1d16727c941598df5bd1e2
guoxiaoqiang <guo_xiaoqiang5@163.com>
2022-08-24 10:31:50 +0800

    学习gitmain

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,7 @@
  * @Author: guoxiaoqiang
  * @Date: 2022-08-16 15:39:04
  * @LastEditors: Please set LastEditors
- * @LastEditTime: 2022-08-23 16:07:58
- * @FilePath: App.jsx
- */
-/*
- * @Descripttion: 
- * @Author: guoxiaoqiang
- * @Date: 2022-07-19 09:50:21
- * @LastEditors: Please set LastEditors
- * @LastEditTime: 2022-08-10 09:53:15
+ * @LastEditTime: 2022-08-24 10:16:38
  * @FilePath: App.jsx
  */
 import React, { Component } from 'react';
